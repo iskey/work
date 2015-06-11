aeba036793f1d8b66924eb5b1cd591278c1e2dcc
xieyichao <xie.yichao@zte.com.cn>
2015-06-11 16:03:34 +0800

    Add array shell test.

---
+++ shell/array.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+AA=(a b c d e f)
+for i in ${AA[@]};
+do
+	echo $i
+done
+
+echo ${AA[@]}
+echo ${AA[2]}
+echo ${AA[3]}
+
+BB=($(ls))
+
+for file in ${BB[@]}
+do
+	echo $file
+done
+
+echo ${AA[*]}
