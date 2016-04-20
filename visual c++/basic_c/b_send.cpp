bc3a15abe75f20387d9104f4d5becdaec049cf3a
xieyc <xie.yichao@zte.com.cn>
2016-04-20 11:02:12 +0800

    t

--- visual c++/basic_c/b_send.cpp
+++ visual c++/basic_c/b_send.cpp
@@ -67,7 +67,7 @@ int isBigEndian()
 
     union MyUnion test;
     test.b = 1;
-    return test.a[1];
+    return test.a[0];
 }
 
 void usage(char *cmd)
@@ -77,69 +77,48 @@ void usage(char *cmd)
 
 #pragma comment(lib,"Ws2_32.lib") 
 
-int main(int argc, char *argv[]) {
-
-    int sock;                   /* socket descriptor */
+int send_ip_set_cmd(char* local_ip, char* remote_ip, char* remote_netmask)
+{
+	int sock;                   /* socket descriptor */
     char send_str[MAX_LEN];     /* string to send */
     struct sockaddr_in mc_addr; /* socket address structure */
     int send_len;               /* length of string to send */
     //char* mc_addr_str;          /* multicast IP address */
     //unsigned short mc_port;     /* multicast port */
     unsigned char mc_ttl = 1;     /* time to live (hop count) */
-    WSADATA wsaData;            /* Windows socket DLL structure */
-
-    /* validate number of arguments */
-    if (argc != 3) {
-    	usage(argv[0]);
-    	exit(1);
-    }
-
-    printf("ip addr is %s, netmask is %s\n", argv[1], argv[2]);
-    //mc_addr_str = argv[1];       /* arg 1: multicast IP address */
-    //mc_port = atoi(argv[2]); /* arg 2: multicast port number */
 
     struct __IP_MASK__ ip_mask;
     BROADCAST_CMD cmd;
-
-    if (isBigEndian()){
-    	ip_mask.ip = inet_addr(argv[1]);
-    	ip_mask.netmask = inet_addr(argv[2]);
-    	cmd.head = CMD_HEAD;
-    	cmd.cmd = CMD_SET_IP;
-    	cmd.data_len = sizeof(struct __IP_MASK__);
+	
+	ip_mask.ip = inet_addr(remote_ip);
+	ip_mask.netmask = inet_addr(remote_netmask);
+   if (isBigEndian()){
+		cmd.head = CMD_HEAD;
+		cmd.cmd = CMD_SET_IP;
+		cmd.data_len = sizeof(struct __IP_MASK__);
     }
     else{
-    	ip_mask.ip = TO_NET32(inet_addr(argv[1]));
-    	ip_mask.netmask = TO_NET32(inet_addr(argv[2]));
-    	cmd.head = TO_NET32(CMD_HEAD);
-    	cmd.cmd = CMD_SET_IP;
-    	cmd.data_len = TO_NET16(sizeof(struct __IP_MASK__));
-    }
-
-
-
-
-    memcpy(cmd.raw_data, &ip_mask, sizeof(ip_mask));
-
-    /* validate the port range */
-    if ((HELLO_PORT < MIN_PORT) || (HELLO_PORT > MAX_PORT)) {
-    	fprintf(stderr, "Invalid port number argument %d.\n",
-    		HELLO_PORT);
-    	fprintf(stderr, "Valid range is between %d and %d.\n",
-    		MIN_PORT, MAX_PORT);
-    	exit(1);
+		cmd.head = TO_NET32(CMD_HEAD);
+		cmd.cmd = CMD_SET_IP;
+		cmd.data_len = TO_NET16(sizeof(struct __IP_MASK__));
     }
 	
-	/* Load Winsock 2.0 DLL */
-	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
-		fprintf(stderr, "WSAStartup() failed");
-		exit(1);
-	}
+    memcpy(cmd.raw_data, &ip_mask, sizeof(ip_mask));
+	
 	
 	/* create a socket for sending to the multicast address */
 	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		perror("socket() failed");
 		exit(1);
+	}
+	
+	SOCKADDR_IN local_addr;
+	local_addr.sin_addr.S_un.S_addr=inet_addr(local_ip);
+	local_addr.sin_family=PF_INET;
+	local_addr.sin_port=htons(15501);
+	
+	if(-1== bind(sock, (SOCKADDR *)& local_addr, sizeof(SOCKADDR))){
+		printf("bind error.\n");
 	}
 	
 	/* set the TTL (time to live/hop count) for the send */
@@ -156,19 +135,69 @@ int main(int argc, char *argv[]) {
 	mc_addr.sin_port = htons(HELLO_PORT);
 	
 	
-	while (1)
+	if (sendto(sock, (const char*)&cmd, sizeof(BROADCAST_CMD), 0, (struct sockaddr *) &mc_addr, sizeof(mc_addr)) < 0)
 	{
-		if (sendto(sock, (const char*)&cmd, sizeof(BROADCAST_CMD), 0, (struct sockaddr *) &mc_addr, sizeof(mc_addr)) < 0)
-		{
-			perror("sendto error.\n");
-			exit(1);
-		}
-		Sleep(1000);
-		printf("command send ok. ip addr is %s, netmask is %s\n", argv[1], argv[2]);
+		perror("sendto error.\n");
+		exit(1);
 	}
+	
+
+	printf("command send ok. ip addr is %s, netmask is %s\n", remote_ip, remote_netmask);
 	
 	
 	closesocket(sock);
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+    char szHostName[128];
+	char szT[20];
+	WSADATA wsaData;            /* Windows socket DLL structure */
+
+    /* validate number of arguments */
+    if (argc != 3) {
+    	usage(argv[0]);
+    	exit(1);
+    }
+
+    printf("ip addr is %s, netmask is %s\n", argv[1], argv[2]);
+    //mc_addr_str = argv[1];       /* arg 1: multicast IP address */
+    //mc_port = atoi(argv[2]); /* arg 2: multicast port number */
+
+	/* validate the port range */
+    if ((HELLO_PORT < MIN_PORT) || (HELLO_PORT > MAX_PORT)) {
+		fprintf(stderr, "Invalid port number argument %d.\n",
+			HELLO_PORT);
+		fprintf(stderr, "Valid range is between %d and %d.\n",
+			MIN_PORT, MAX_PORT);
+		exit(1);
+    }
+	
+	/* Load Winsock 2.0 DLL */
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+		fprintf(stderr, "WSAStartup() failed");
+		exit(1);
+	}
+
+	if( gethostname(szHostName, 128) == 0 )
+    {
+        // Get host adresses
+        struct hostent * pHost; 
+        int i;  
+        pHost = gethostbyname(szHostName); 
+        for( i = 0; pHost!= NULL && pHost->h_addr_list[i]!= NULL; i++ )  
+        {
+			char str[100];         
+			char addr[20];
+			int j;         
+			char * eth_name=inet_ntoa (*(struct in_addr *)pHost->h_addr_list[i]);
+			send_ip_set_cmd(eth_name, argv[1], argv[2]);
+        }
+    }
+
 	WSACleanup();  /* Cleanup Winsock */
 	
 	return 0;
